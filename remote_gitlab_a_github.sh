fa73e47c69c0beda116bac8930e2a439ea79b7cf
brian mengibar <brianmengibargarcia@gmail.com>
2017-03-20 09:42:30 +0100

    Update file sh

--- remote_gitlab_a_github.sh
+++ remote_gitlab_a_github.sh
@@ -1,1 +1,13 @@
+#! /bin/bash
+
+#Nom autor: Brian Mengibar
+#Identificador: isx39441584
+#Data: 16/03/2017
+#Cicle Formatiu: Administracio de sistemes informatics en xarxes
+#Modul Profesional: MP9
+#Unitat formativa: MP9UF2 
+
+#Script de nom remote_gitlab_a_github.sh per replicar el teu repositori
+#de gitlab a github.
+
 for r in $(git remote); do git push $r master; done
